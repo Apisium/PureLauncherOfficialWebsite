18cfa1e1ee33265aebc21983c2f2f4cff0c7df8c
Shirasawa <764798966@qq.com>
2020-02-26 19:58:57 +0800

    deploy

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@ const data: { os: { name?: string }, cpu: { architecture?: string } } = new (win
 
 const os = (data.os.name || '').toLowerCase()
 
-let text: string
-if (os.startsWith('android') || os.startsWith('blackberry') || os.startsWith('ios') || os.startsWith('windows phone')) {
-  text = $i('notSupport')
-  $('#download-now').prop('disabled', true)
-} else if (os.startsWith('windows')) text = `Windows ${$i((data.cpu.architecture || '').includes('64') ? '64' : '32')} (.exe)`
-else if (os.startsWith('mac os')) text = `MACOS ${$i('64')} (.dmg)`
-else if (os.startsWith('debian') || os.startsWith('ubuntu') || os.startsWith('deepin')) text = `Debian Linux ${$i('64')} (.deb)`
-else if (os.startsWith('redhat') || os.startsWith('suse') || os.startsWith('centos')) text = `RedHat Linux ${$i('64')} (.rpm)`
-else text = `Linux ${$i('64')} (.tar.gz)`
-$('#release-type').text(text)
+const changeText = () => {
+  let text: string
+  if (os.startsWith('android') || os.startsWith('blackberry') || os.startsWith('ios') || os.startsWith('windows phone')) {
+    text = $i('notSupport')
+    $('#download-now').prop('disabled', true)
+  } else if (os.startsWith('windows')) text = `Windows ${$i((data.cpu.architecture || '').includes('64') ? '64' : '32')} (.exe)`
+  else if (os.startsWith('mac os')) text = `MACOS ${$i('64')} (.dmg)`
+  else if (os.startsWith('debian') || os.startsWith('ubuntu') || os.startsWith('deepin')) text = `Debian Linux ${$i('64')} (.deb)`
+  else if (os.startsWith('redhat') || os.startsWith('suse') || os.startsWith('centos')) text = `RedHat Linux ${$i('64')} (.rpm)`
+  else text = `Linux ${$i('64')} (.tar.gz)`
+  $('#release-type').text(text)
+}
+(window as any).changeText = changeText
+changeText()
